75e1c3eba69fd7c2c5adaaa1d3a56044fdaa184c
arolithiya0805-collab <arolithiya0805@gmail.com>
2026-07-01 00:05:10 +0530

    Update Problem13.java

---
+++ Day6/Problem13.java
@@ -0,0 +1,11 @@
+package Day6;
+
+public class Problem13 {
+    public static void main(String[] args) {
+         double a = 99.9;
+        short b = (short)a;
+
+        System.out.println(b);
+    }
+    
+}
