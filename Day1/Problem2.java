2a53a3ecfb7ddcffa4cf196d4cdb12f3335b4c8b
arolithiya0805-collab <arolithiya0805@gmail.com>
2026-07-01 00:21:20 +0530

    Update Problem2.java

--- Day1/Problem2.java
+++ Day1/Problem2.java
@@ -1,6 +1,6 @@
 package Day1;
 
-public class Problem5 {
+public class Problem2 {
     public static void main(String[] args) {
         System.out.println(System.getProperty("java.version"));
     }
