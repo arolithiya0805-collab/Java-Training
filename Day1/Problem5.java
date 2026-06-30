82b2bb25204ecdfa78c8a7ec07816b8cfd036ac2
arolithiya0805-collab <arolithiya0805@gmail.com>
2026-07-01 00:23:21 +0530

    Update Problem5.java

--- Day1/Problem5.java
+++ Day1/Problem5.java
@@ -1,6 +1,6 @@
 package Day1;
 
-public class Problem8 {
+public class Problem5 {
     public static void main(String[] args) {
         System.out.println(Runtime.getRuntime().totalMemory());
     }
