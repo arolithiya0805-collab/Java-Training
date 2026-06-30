f2f8068356b032f848e9114cd7802d8766cc13ca
arolithiya0805-collab <arolithiya0805@gmail.com>
2026-07-01 00:14:52 +0530

    Update Problem6.java

---
+++ Day1/Problem6.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class Problem9 {
+    static void show() {
+        System.out.println("Method");
+    }
+
+    public static void main(String[] args) {
+        show();
+    }
+}
