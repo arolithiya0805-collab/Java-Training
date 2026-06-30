d8d04e7cebac32af77eb2657ab403721be8a3809
arolithiya0805-collab <arolithiya0805@gmail.com>
2026-07-01 00:16:09 +0530

    Update Problem9.java

---
+++ Day1/Problem9.java
@@ -0,0 +1,11 @@
+package Day1;
+
+public class Problem12 {
+    static {
+        System.out.println("Static Block");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Main");
+    }
+}
