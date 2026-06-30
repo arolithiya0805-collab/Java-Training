c2d59ba128c6f4b267df8528c6e269c88ddb9037
arolithiya0805-collab <arolithiya0805@gmail.com>
2026-07-01 00:23:01 +0530

    Update Problem4.java

--- Day1/Problem4.java
+++ Day1/Problem4.java
@@ -1,6 +1,6 @@
 package Day1;
 
-public class Problem7 {
+public class Problem4 {
     public static void main(String[] args) {
         System.out.println(System.getProperty("java.vm.name"));
     }
