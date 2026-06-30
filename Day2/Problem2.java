3fb3c6f17b78fb6b234a840b084fa315e8bba989
arolithiya0805-collab <arolithiya0805@gmail.com>
2026-07-01 00:34:26 +0530

    Update Problem2.java

--- Day2/Problem2.java
+++ Day2/Problem2.java
@@ -1,6 +1,6 @@
 package Day2;
 
-public class Problem3 {
+public class Problem2 {
     public static void main(String[] args) {
         int a = 10, b = 20, c = 30;
         System.out.println((a + b + c) / 3);
