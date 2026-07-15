f38be0ec6a37b204361891d4cb8cfd6a2619cb4c
arolithiya0805 <arolithiya@gmail.com>
2026-07-15 18:19:52 +0530

    Initial

---
+++ Day24/Problem1.java
@@ -0,0 +1,25 @@
+package Day24;
+
+public class Problem1 {
+    class Solution {
+    public int longestSubarray(int[] nums) {
+        int left = 0, zeroCount = 0, max = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] == 0)
+                zeroCount++;
+
+            while (zeroCount > 1) {
+                if (nums[left] == 0)
+                    zeroCount--;
+                left++;
+            }
+
+            max = Math.max(max, right - left);
+        }
+
+        return max;
+    }
+}
+    
+}
